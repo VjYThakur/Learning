f0bcfe3de24bc188d4ac3c2c6255dd4e7560772e
VjYthakur <vijay.vjthakur.thakur@gmail.com>
2024-07-19 00:03:39 +0530

    new commit

--- script.js
+++ script.js
@@ -79,3 +79,34 @@ function updateDashboardMetrics() {
 
 // Initial update of dashboard metrics on page load
 updateDashboardMetrics();
+
+// Script for user section:
+
+function addOption() {
+  const userType = document.getElementById("userType");
+  if (userType.value === "Select") {
+    alert("Please select correct option");
+  } else {
+    console.log(userType.options[userType.selectedIndex].value);
+  }
+  document.getElementById("userType").value = "Select";
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
